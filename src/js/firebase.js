07f5fec9d9ba32a41346fa190f42c2dca5eb9bdc
changbenny <changbenny0401@gmail.com>
2016-04-02 18:52:31 +0800

    fix the remove bug

--- src/js/firebase.js
+++ src/js/firebase.js
@@ -17,6 +17,4 @@ function fromFirebase(app = 'test') {
     firebase.child(app).on('value', snapshot => resolve(snapshot.val()))
   })
 }
-firebase.remove();
-console.log('remove');
 export { toFirebase, fromFirebase };
